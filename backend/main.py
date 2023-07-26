a1b805b937ddf1dcbc5d0fc07e0d2ac07cf585e3
Osman-MUTLU <osman_mutlu_2205@hotmail.com>
2023-07-26 14:54:10 +0300

    upload

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,27 @@
-print("main")
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+class Promt(BaseModel):
+    text: str
+    token: str
+
+app = FastAPI()
+
+
+@app.get("/promt")
+async def root(promt: Promt):
+    token = promt.token
+    # token kullanarak microsoft graph api ile mailleri çek
+    
+    mails = requests.get("https://graph.microsoft.com/v1.0/me/messages", headers={
+        "Authorization": "Bearer " + token,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Prefer": "outlook.body-content-type='text'"
+        })
+    mails = mails.json()
+    mails = mails["value"]
+    # mailleri tek tek dolaşarak içeriklerini çek
+    return mails
+
